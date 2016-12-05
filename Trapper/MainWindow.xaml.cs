30afa4973e5564860592719dcb0eb13ebb83f6bb
Robert Gott <robgott@yahoo.com>
2016-12-05 12:09:00 -0500

    Cleaned solution

---
+++ Trapper/MainWindow.xaml.cs
@@ -0,0 +1,309 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using Forms = System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Controls;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+
+namespace Trapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // TODO: Add background color / image
+        // TODO: Add icon file change
+        // TODO: Add settings
+        // TODO: improve proformance by not polling mouse
+        // TODO: add movement event to title bar
+        #region Keyboard and Mouse Hooks
+
+        private IKeyboardMouseEvents m_GlobalHook;
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseUpExt += GlobalHookMouseUpExt;
+        }
+        private void GlobalHookMouseUpExt(object sender, MouseEventExtArgs e)
+        {
+            IntPtr current = Win32.GetForegroundWindow();
+            Win32.WinDocPos posHolder;
+            if ((posHolder = isInWindow(e)) != Win32.WinDocPos.outOfWindow)
+            {
+                if (current != windowPtr) // and not this(MainWindow) window
+                    AddWindow(new WindowContainer(windowPtr, current, posHolder));// add to list
+            }
+            else
+            {
+                RemoveWindow(new WindowContainer(windowPtr, current)); // try to remove window if exists
+            }
+        }
+       
+        private Win32.WinDocPos isInWindow(MouseEventExtArgs e)
+        {
+            int X = e.X;
+            int Y = e.Y;
+            if ((X > Left && X < Width + Left) && (Y > Top && Y < Top + Height))
+            { // in window
+                int space = 15;
+                if((X > Left + space && X < Width + Left - space) && (Y > Top + space && Y < Top + Height - space))// main
+                { 
+                    return Win32.WinDocPos.main;
+                }
+                else if((X > Left && X < Left + space) && (Y > Top + space && Y < Top + Height - space))// left
+                {
+                    return Win32.WinDocPos.Left;
+                }
+                else if ((X > Left + space && X < Width + Left - space) && (Y > Top && Y < Top + space))// Top
+                {
+                    return Win32.WinDocPos.Top;
+                }
+                else if ((X > Width + Left - space && X < Width + Left) && (Y > Top + space && Y < Top + Height - space))// right
+                {
+                    return Win32.WinDocPos.Right;
+                }
+                else if ((X > Left + space && X < Width + Left - space) && (Y > Top + Height - space && Y < Top + Height))// bottom
+                {
+                    return Win32.WinDocPos.Bottom;
+                }
+                else if ((X > Left && X < Left + space) && (Y > Top && Y < Top + space))// TopLeft
+                {
+                    return Win32.WinDocPos.TopLeft;
+                }
+                else if ((X > Left + Width - space && X < Width + Left) && (Y > Top && Y < Top + space))// TopRight
+                {
+                    return Win32.WinDocPos.TopRight;
+                }
+                else if ((X > Left + Width - space && X < Width + Left) && (Y > Top + Height - space && Y < Top + Height))// BottomRight
+                {
+                    return Win32.WinDocPos.BottomRight;
+                }
+                else if ((X > Left && X < Left + space) && (Y > Top + Height - space && Y < Top + Height))// BottomLeft
+                {
+                    return Win32.WinDocPos.BottomLeft;
+                }
+            }
+            return Win32.WinDocPos.outOfWindow;
+        }
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseUpExt;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+        #endregion
+
+        IntPtr windowPtr;
+        List<Window> allWindows = new List<Window>();
+        List<WindowContainer> window = new List<WindowContainer>();
+        #region extentions to WindowContainerList
+        private void AddWindow(WindowContainer win)
+        {
+            int index;
+            if ((index = window.IndexOf(win)) == -1)
+            {
+                if (!Properties.Settings.Default.TabSwitchVisible)
+                    win.setTabSwitchVisible(false);
+                window.Add(win);
+            }
+            else
+            {
+                switch (win.windowDockLocation)
+                {
+                    case Win32.WinDocPos.main:
+                        window[index].offset = win.offset; // reset offset if tried to add again
+                        break;
+                    case Win32.WinDocPos.TopLeft:
+                    case Win32.WinDocPos.Top:
+                    case Win32.WinDocPos.TopRight:
+                    case Win32.WinDocPos.Right:
+                    case Win32.WinDocPos.BottomRight:
+                    case Win32.WinDocPos.Bottom:
+                    case Win32.WinDocPos.BottomLeft:
+                    case Win32.WinDocPos.Left:
+                        window[index].setWindow();
+                        break;
+                }
+            }
+        }
+        public void RemoveWindow(WindowContainer win)
+        {
+            int index;
+            if((index = window.IndexOf(win)) != -1)
+            {
+                //if(!Properties.Settings.Default.TabSwitchVisible)
+                //{
+                    win.setTabSwitchVisible(true);
+                    window[index].setTabSwitchVisible(true);
+
+                //}
+                window.RemoveAt(index);
+            }
+        }
+        #endregion
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Trapper.Background = SystemColors.HotTrackBrush;
+
+            AppSwitcher.IsChecked = Properties.Settings.Default.TabSwitchVisible;
+            Subscribe(); // get all global key events sent to program
+            bool slower = true;
+            if(slower)
+            { // load additional methods
+                this.LocationChanged += Trapper_LocationChanged;
+            }
+        }
+        
+        private void Trapper_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void ExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Trapper_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            foreach (WindowContainer item in window)
+            {
+                item.setWindow();
+            }
+        }
+
+        private void Trapper_LocationChanged(object sender, EventArgs e)
+        {
+            if (window != null && Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                new Thread(() =>
+                {
+                    foreach (WindowContainer win in window)
+                        win.setWindow();
+                }).Start();
+            }
+        }
+
+        private void Trapper_Loaded(object sender, RoutedEventArgs e)
+        {
+            windowPtr = new WindowInteropHelper(Application.Current.MainWindow).EnsureHandle();
+        }
+
+        private void Trapper_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Properties.Settings.Default.TabSwitchVisible)
+            {
+                foreach (WindowContainer item in window)
+                {
+                    item.setTabSwitchVisible(true);
+                    item.resetWindow();
+                }
+            }
+            else
+            {
+                foreach (WindowContainer item in window)
+                    item.resetWindow();
+            }
+
+            
+            Unsubscribe();
+        }
+
+        private void Trapper_StateChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Normal:
+                case WindowState.Maximized:
+                    new Thread(() => {
+                        foreach (WindowContainer item in window)
+                        {
+                            item.showWindow();
+                        }
+                    }).Start();
+                    break;
+                case WindowState.Minimized:
+                    new Thread(() => {
+                        foreach (WindowContainer item in window)
+                        {
+                            item.setMinimizeWindow();
+                            item.resetWindow();
+                        }
+                    }).Start();
+                    break;
+            }
+        }
+
+        private void Trapper_Activated(object sender, EventArgs e)
+        {
+            foreach (WindowContainer item in window)
+            {
+                item.setWindow();
+            }
+        }
+
+        private void Trapper_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (window != null && Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                new Thread(() => {
+                    foreach (WindowContainer win in window)
+                        win.setWindow();
+                }).Start();
+            }
+            // TODO Validation and auto resizing of inner windows
+        }
+
+        private void TitleBar_Click(object sender, RoutedEventArgs e)
+        {
+            if (((MenuItem)sender).IsChecked)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.None;
+            }
+        }
+
+        private void pinMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (((MenuItem)sender).IsChecked)
+            {
+                this.Topmost = true;
+            }
+            else
+            {
+                this.Topmost = false;
+            }
+        }
+
+        private void AppSwitcher_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.TabSwitchVisible = ((MenuItem)sender).IsChecked;
+            Properties.Settings.Default.Save();
+
+            foreach (WindowContainer item in window)
+            {
+                item.setTabSwitchVisible(Properties.Settings.Default.TabSwitchVisible);
+            }
+        }
+    }
+}
---
+++ Trapper/WindowContainer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace Trapper
+{
+    public class WindowContainer
+    {
+        public System.Windows.Point offset;
+        public IntPtr childWindowHandle;
+        public Win32.Rect childWindowRect;
+
+        public IntPtr mainWindowHandle;
+        public Win32.Rect mainWindowRect;
+        public Win32.WinDocPos windowDockLocation;
+        public WindowContainer(IntPtr mainWindow, IntPtr windowHandle, Win32.WinDocPos windowDockLocation) : this(mainWindow, windowHandle)
+        {
+            this.windowDockLocation = windowDockLocation;
+        }
+        public WindowContainer(IntPtr mainWindow, IntPtr windowHandle)
+        {
+            this.childWindowHandle = windowHandle;
+            this.mainWindowHandle = mainWindow;
+
+            childWindowRect = new Win32.Rect();
+            Win32.GetWindowRect(windowHandle, ref childWindowRect);
+            Win32.GetWindowRect(mainWindow, ref mainWindowRect);
+
+            offset.X = childWindowRect.Left - mainWindowRect.Left;
+            offset.Y = childWindowRect.Top - mainWindowRect.Top;
+        }
+        public void setWindow()
+        {
+            Win32.Rect main = new Win32.Rect();
+            Win32.GetWindowRect(mainWindowHandle, ref main);
+            childWindowRect.Left = main.Left;
+            childWindowRect.Top = main.Top;
+
+            if (windowDockLocation != Win32.WinDocPos.main)
+            { // these windows require sizing
+                int WTop = childWindowRect.Top;
+                int WLeft = childWindowRect.Left;
+                int WWidth = 1000;
+                int WHeight = 1000;
+                switch (windowDockLocation) // set size
+                {
+                    case Win32.WinDocPos.TopLeft:
+                    case Win32.WinDocPos.TopRight:
+                    case Win32.WinDocPos.BottomLeft:
+                    case Win32.WinDocPos.BottomRight:
+                        WWidth = main.Width / 2;
+                        WHeight = main.Height / 2;
+                        break;
+                    case Win32.WinDocPos.Top:
+                    case Win32.WinDocPos.Bottom:
+                        WWidth = main.Width;
+                        WHeight = main.Height / 2;
+                        break;
+                    case Win32.WinDocPos.Right:
+                    case Win32.WinDocPos.Left:
+                        WWidth = main.Width / 2;
+                        WHeight = main.Height;
+                        break;
+                }
+                switch (windowDockLocation)
+                {
+                    case Win32.WinDocPos.TopLeft:
+                    case Win32.WinDocPos.Left:
+                    case Win32.WinDocPos.Top:
+                        // Already in pos
+                        break;
+                    case Win32.WinDocPos.Bottom:
+                    case Win32.WinDocPos.BottomLeft:
+                        WTop += main.Height / 2;
+                        break;
+                    case Win32.WinDocPos.TopRight:
+                    case Win32.WinDocPos.Right:
+                        WLeft += main.Width / 2;
+                        break;
+                    case Win32.WinDocPos.BottomRight:
+                        WTop += main.Height / 2;
+                        WLeft += main.Width / 2;
+                        break;
+                }
+
+                Win32.SetWindowPos(
+                        childWindowHandle,
+                        Win32.HWND_TOPMOST,
+                        WLeft,
+                        WTop,
+                        WWidth,
+                        WHeight,
+                        Win32.SWP_SHOWWINDOW | Win32.SWP_NOACTIVATE);
+            }
+            else
+            {
+                Win32.SetWindowPos(
+                        childWindowHandle,
+                        Win32.HWND_TOPMOST,
+                        (int)(childWindowRect.Left + offset.X),
+                        (int)(childWindowRect.Top + offset.Y),
+                        0,
+                        0,
+                        Win32.SWP_SHOWWINDOW | Win32.SWP_NOSIZE | Win32.SWP_NOACTIVATE);
+            }
+            resetWindow();
+        }
+        public void resetWindow()
+        {
+            Win32.SetWindowPos(
+                childWindowHandle,
+                Win32.HWND_NOTOPMOST,
+                0, 0, 0, 0,
+                Win32.SWP_SHOWWINDOW | Win32.SWP_NOSIZE | Win32.SWP_NOMOVE | Win32.SWP_NOACTIVATE);
+        }
+        public void setTop()
+        {
+            Win32.SetWindowPos(
+                childWindowHandle,
+                Win32.HWND_TOPMOST,
+                0, 0, 0, 0,
+                Win32.SWP_SHOWWINDOW | Win32.SWP_NOSIZE | Win32.SWP_NOMOVE | Win32.SWP_NOZORDER | Win32.SWP_NOACTIVATE);
+        }
+        public void setMinimizeWindow()
+        {
+            Win32.ShowWindow(childWindowHandle, Win32.SW_MINIMIZE);
+        }
+        public void showWindow()
+        {
+            Win32.ShowWindow(childWindowHandle, Win32.SW_SHOWNOACTIVATE);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is WindowContainer)
+            {
+                WindowContainer win = obj as WindowContainer;
+                if (win.childWindowHandle == this.childWindowHandle)
+                    return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public void setTabSwitchVisible(bool val = true)
+        {
+            int exStyle = (int)Win32.GetWindowLong(childWindowHandle, (int)Win32.GetWindowLongFields.GWL_EXSTYLE);
+            if (!val) 
+            { // adds tool window style
+                exStyle |= (int)Win32.WindowStylesEx.WS_EX_TOOLWINDOW;
+            }
+            else
+            { // removes the tool window style
+                exStyle -= (int)Win32.WindowStylesEx.WS_EX_TOOLWINDOW;
+            }
+            Win32.SetWindowLong(childWindowHandle, (int)Win32.GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+
+    }
+}
